3b85cd7aa929b94648a52f509cd51c2909e6c464
Michael Cockcroft <cocymsc@hotmail.com>
2022-07-04 11:50:22 +0100

    add mutation tests

--- packages/client/src/app/pages/Banks/components/AddBankPartnerTab.test.js
+++ packages/client/src/app/pages/Banks/components/AddBankPartnerTab.test.js
@@ -1,11 +1,10 @@
 import { MockedProvider } from '@apollo/client/testing';
 import { screen, render, waitFor } from '@testing-library/react';
-
+import userEvent from '@testing-library/user-event';
+import { CREATE_BANK_PARTNER } from '../gql/mutations';
 import { GET_BANK_PARTNER_DATA } from '../gql/queries';
 
 import { AddBankPartnerTab } from './';
-
-const bankPartnerCall = jest.fn();
 
 const props = {
   currentBankDid: 'test-did',
@@ -21,23 +20,36 @@ const defaultMocks = [
         currency: 'GBP',
       },
     },
-    result: () => {
-      bankPartnerCall();
-      return {
-        data: {
-          getForeignBanks: [
-            {
-              bankDid: 'RTGS:B:GB13951280',
-              bankName: 'cypressTestBank678362',
-              currency: 'GBP',
-            },
-          ],
-          getUser: {
-            supportedCurrencies: ['GBP', 'USD', 'JPY', 'EUR'],
+    result: {
+      data: {
+        getForeignBanks: [
+          {
+            bankDid: 'RTGS:B:EU13951280',
+            bankName: 'cypressTestBank678362',
+            currency: 'EUR',
+          },
+        ],
+        getUser: {
+          supportedCurrencies: ['GBP', 'USD', 'JPY', 'EUR'],
+        },
+      },
+    },
+  },
+  {
+    request: {
+      query: CREATE_BANK_PARTNER,
+      variables: {
+        values: {
+          currency: 'GBP',
+          bankPartner: {
+            approvingBankDid: 'RTGS:B:EU13951280',
+            iban: 'GB14567456',
+            requestingBankDid: 'test-did',
           },
         },
-      };
+      },
     },
+    result: { data: { createBankPartner: { status: 'Success' } } },
   },
 ];
 
@@ -49,22 +61,52 @@ const renderComponent = (mocks = defaultMocks) => {
   );
 };
 
+const completeAndSubmitForm = async () => {
+  const currencyField = screen.getByTestId('currency-input');
+  const availableBanksField = screen.getByTestId('bank-input');
+  const ibanField = screen.getByTestId('bank-iban-input');
+
+  userEvent.type(currencyField, 'E');
+  await userEvent.click(screen.getByTestId('currency-input-optionEUR'));
+
+  await userEvent.type(availableBanksField, 'cypress');
+  await userEvent.click(
+    screen.getByTestId('bank-input-optionRTGS:B:EU13951280')
+  );
+
+  await userEvent.type(ibanField, 'GB14567456');
+
+  userEvent.click(screen.getByRole('button', { name: 'Add Bank Partner' }));
+};
+
 describe('AddBankPartnerTab', () => {
-  beforeEach(() => {
-    jest.resetAllMocks();
-  });
-
-  it('should query GET_BANK_PARTNER_DATA on load', async () => {
-    renderComponent();
-    await waitFor(() => expect(bankPartnerCall).toHaveBeenCalled());
-  });
-
   it('should render add bank partner form', async () => {
     renderComponent();
 
     await waitFor(() =>
       expect(screen.getByText('Add Bank Partner')).toBeInTheDocument()
     );
+  });
+
+  it('should populate dropdowns with GET_BANK_PARTNER_DATA query data', async () => {
+    renderComponent();
+
+    await waitFor(() =>
+      expect(
+        screen.queryByTestId('add-bank-partner-loader')
+      ).not.toBeInTheDocument()
+    );
+
+    userEvent.click(screen.getByTestId('currency-input'));
+
+    expect(screen.getByTestId('currency-input-optionEUR')).toBeInTheDocument();
+
+    await userEvent.click(screen.getByTestId('currency-input-optionEUR'));
+    await userEvent.type(screen.getByTestId('bank-input'), 'cypress');
+
+    expect(
+      screen.getByTestId('bank-input-optionRTGS:B:EU13951280')
+    ).toBeInTheDocument();
   });
 
   it('should render loader when bank partner data is loading', () => {
@@ -92,5 +134,60 @@ describe('AddBankPartnerTab', () => {
     await waitFor(() =>
       expect(screen.getByText('Error retrieving form data')).toBeInTheDocument()
     );
+  });
+
+  describe('when submitting form', () => {
+    it('should display success modal on successful response', async () => {
+      renderComponent();
+
+      await waitFor(() =>
+        expect(
+          screen.queryByTestId('add-bank-partner-loader')
+        ).not.toBeInTheDocument()
+      );
+
+      await completeAndSubmitForm();
+
+      await waitFor(() => {
+        expect(
+          screen.getByTestId('success-bank-partner-alert')
+        ).toBeInTheDocument();
+      });
+    });
+
+    it('should display error modal on error response', async () => {
+      const createBankPartnerErrorMock = {
+        request: {
+          query: CREATE_BANK_PARTNER,
+          variables: {
+            values: {
+              currency: 'GBP',
+              bankPartner: {
+                approvingBankDid: 'RTGS:B:EU13951280',
+                iban: 'GB14567456',
+                requestingBankDid: 'test-did',
+              },
+            },
+          },
+        },
+        result: { data: { createBankPartner: { status: 'Failed' } } },
+      };
+
+      renderComponent([defaultMocks[0], createBankPartnerErrorMock]);
+
+      await waitFor(() =>
+        expect(
+          screen.queryByTestId('add-bank-partner-loader')
+        ).not.toBeInTheDocument()
+      );
+
+      await completeAndSubmitForm();
+
+      await waitFor(() => {
+        expect(
+          screen.getByTestId('error-bank-partner-alert')
+        ).toBeInTheDocument();
+      });
+    });
   });
 });
